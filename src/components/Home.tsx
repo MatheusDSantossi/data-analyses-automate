30ef4b6ca0869085a35211e58f5d3e16898c65ae
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-16 20:08:01 -0300

    feat(Home): improve the design with new colors and structure, plus I add the current system logo (test)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,25 @@
 const Home = () => {
   return (
-    <main>
-      <header className="mb-5">
-        <h1>Welcome to our Automate Data Analyse Wizard Tool</h1>
-        <h4>Select the file to start the magic</h4> 
-        {/* Add some magic here (stars or something) */}
-      </header>
-      <section>
-        {/* <label htmlFor="data_file" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select the file!!</label> */}
-        <input type="file" className="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 hover:bg-amber-400 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="data_file" />
-      </section>
-    </main>
+    <div>
+      <div className="top-0">
+        <img className="h-16" src="/src/assets/logo.png" alt="System Logo" />
+      </div>
+      <main className="flex flex-col justify-center items-center h-screen">
+        <header className="flex flex-col items-start mb-16">
+          <h1 className="font-medium text-2xl">Welcome to our Automate Data Analyse Wizard Tool</h1>
+          <h4>Select the file to start the magic</h4>
+          {/* Add some magic here (stars or something) */}
+        </header>
+        <section>
+          {/* <label htmlFor="data_file" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select the file!!</label> */}
+          <input
+            type="file"
+            className="block w-full mb-5 p-2 text-sm text-primary border border-gray-300 rounded-lg cursor-pointer bg-gray-700 hover:bg-white-200 dark:text-secondary-dark focus:outline-none dark:bg-white dark:border-gray-600 dark:placeholder-gray-400"
+            id="data_file"
+          />
+        </section>
+      </main>
+    </div>
   );
 };
 
