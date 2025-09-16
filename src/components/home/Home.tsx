a7776566d483550d717c67cdf5c5c7590dda15d8
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-16 20:53:56 -0300

    feat: KendoReact input added and file selection process handler started

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import "./Home.css";
+import { Input, type InputChangeEvent } from "@progress/kendo-react-inputs";
 
 const Home = () => {
+  // State
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  // Function to handle the selected file and work with it
+  const handleFileChange = (event: InputChangeEvent) => {
+    console.log("event.target.files: ", event.target);
+
+    if (event.target.element?.files && event.target.element?.files.length > 0) {
+      setSelectedFile(event.target.element?.files[0]);
+      console.log(event.target.element?.files[0].name);
+    } else {
+      alert("You must select a file");
+    }
+  };
+
   return (
     <div>
       <div className="top-0">
@@ -9,17 +26,22 @@ const Home = () => {
       <main className="flex flex-col justify-center items-center h-screen">
         <header className="flex flex-col items-start mb-16">
           <h1 className="font-medium text-2xl">
-            Welcome to our <span className="highlight c3">Automate Data Analyse Wizard Tool</span>
+            Welcome to our{" "}
+            <span className="highlight c3">
+              Automate Data Analyse Wizard Tool
+            </span>
           </h1>
           <h4>Select the file to start the magic</h4>
           {/* Add some magic here (stars or something) */}
         </header>
         <section>
           {/* <label htmlFor="data_file" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select the file!!</label> */}
-          <input
-            type="file"
+
+          <Input
             className="block w-full mb-5 p-2 text-sm text-primary border border-gray-300 rounded-lg cursor-pointer bg-gray-700 hover:bg-white-200 dark:text-secondary-dark focus:outline-none dark:bg-white dark:border-gray-600 dark:placeholder-gray-400"
-            id="data_file"
+            type="file"
+            id="file_select"
+            onChange={handleFileChange}
           />
         </section>
       </main>
