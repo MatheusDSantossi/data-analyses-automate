61570a5f231a771e6122e0d570590d9bf6576fc7
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-16 21:05:39 -0300

    feat(Dashabord, routes): scaffold for Dashboard screen and finished the structure for routes behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
 
 function App() {
   return (
     <>
-      <div>
-        <Home />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
