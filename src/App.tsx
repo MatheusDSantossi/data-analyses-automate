a55137b71f841fe4fc886fdb1a05f1a538d573f4
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-17 21:09:00 -0300

    feat: integrate file context across components - Wrapped app with FileProvider in main.tsx - Updated global styles theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { useFile } from "../../context/FileContext";
+import { useEffect, useState } from "react";
+
+import * as XLSX from "xlsx";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  // File Context
+  const { file, parsedData, setParsedData } = useFile();
+
+  const navigate = useNavigate();
+
+  // States
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!file) {
+      // If no file in context, send user back to Home
+      navigate("/");
+      return;
+    }
+
+    // If parsedData already exists skip parsing
+    if (parsedData) return;
+
+    const parse = async () => {
+      try {
+        setLoading(true);
+
+        const arrayBuffer = await file.arrayBuffer();
+        // Detect file type by extension
+        const filename = file.name.toLowerCase();
+        if (filename.endsWith(".csv")) {
+          // csv -> read as text
+          const text = new TextDecoder().decode(arrayBuffer);
+          const rows = csvToJson(text); // helper
+          setParsedData(rows);
+        } else {
+          // xslx/xlsx... -> we use SheetJS
+          const workbook = XLSX.read(arrayBuffer, { type: "array" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const rows = XLSX.utils.sheet_to_json(worksheet, { defval: null });
+
+          setParsedData(rows);
+        }
+      } catch (err) {
+        console.log("Parsing error", err);
+        alert("Error parsing file. See console for details;");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    parse();
+  }, [file, parsedData, navigate, setParsedData]);
+
+  if (!file) return null; // redurect handle above
+
+  return <div>{file?.name}</div>;
+};
+
+function csvToJson(csv: string) {
+  // const lines = csv.split()
 }
 
-export default Dashboard
+export default Dashboard;
